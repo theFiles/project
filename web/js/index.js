556ee96364df7b90778e7ad826667afcf247e3a8
lidaye <541725460@qq.com>
2019-07-16 10:34:03 +0800

    登录页面验证完成

--- web/js/index.js
+++ web/js/index.js
@@ -1,65 +1,68 @@
 var userFocus = function(){
-    var val = this.value;
-    var iniVal = this.getAttribute('ini-val');
-    if(val == iniVal){
+    if(this.value === this.getAttribute('ini-val')){
         this.value = '';
     }
-}
+};
 
 var userBlur = function(){
-    var val = this.value;
-    var iniVal = this.getAttribute('ini-val');
-
-    if(val == ''){
-        this.value = iniVal;
+    if(this.value){
+        this.value = this.getAttribute('ini-val');
     }
-}
+};
 
 var pwdFocus = function(){
-    var val = this.value;
-    var iniVal = this.getAttribute('ini-val');
-
-    if(val == iniVal){
+    if(this.value === this.getAttribute('ini-val')){
         this.setAttribute('type','password');
         this.value = '';
     }
-}
+};
 
 var pwdBlur = function(){
-    var val = this.value;
-    var iniVal = this.getAttribute('ini-val');
-
-    if(val == ''){
+    if(this.value){
         this.setAttribute('type','text');
-        this.value = iniVal;
+        this.value = this.getAttribute('ini-val');
     }
-}
+};
 
+/**
+ * 提交验证
+ */
 var formSubmit = function(){
-    var userValue = document.getElementById('user-ipt'),
-        userIni   = userValue.getAttribute('ini-val'),
-        pwdValue  = document.getElementById('pwd-ipt'),
-        pwdIni    = pwdValue.getAttribute('ini-val');
+    var user      = document.getElementById('user-ipt'),
+        userVal   = user.value,
+        userIni   = user.getAttribute('ini-val'),
+        pwd       = document.getElementById('pwd-ipt'),
+        pwdVal    = pwd.value,
+        pwdIni    = pwd.getAttribute('ini-val');
 
     if(
-        userValue    != ''
-        || pwdValue  != ''
-        || userValue != userIni
-        || pwdValue  != pwdIni
+        userVal
+        || pwdVal
+        || userVal === userIni
+        || pwdVal  === pwdIni
     ){
         alert('账户密码不能为空！');
         return false;
     }
 
-}
+};
 
+/**
+ * 密码框
+ */
 var pwd = document.getElementById('pwd-ipt');
 pwd.onblur = pwdBlur;
 pwd.onfocus = pwdFocus;
 
+/**
+ * 用户框
+ */
 var user = document.getElementById('user-ipt');
 user.onblur = userBlur;
 user.onfocus = userFocus;
 
+/**
+ * 提交按钮
+ */
 var subBtn = document.getElementById('sub-btn');
 subBtn.onclick = formSubmit;
